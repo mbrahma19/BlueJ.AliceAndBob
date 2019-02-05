18e6882fe7399f7c7dc93d7e7f3cdf9d2cbab95d
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-8.local>
2019-02-05 11:49:55 -0500

    Coded main function to include user input and print statements

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("Hello! What is your name?");
+        
+        String username = userInput.nextLine();
+        
+        if (username.equals("Alice") || username.equals("Bob")){
+            System.out.println("Great to see you " + username + "!");
+        }
+        else{
+            System.out.println("I am not looking for you");
+        }
 
     }
 }
